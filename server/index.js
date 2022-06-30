a88dc8be1997d146d21361617500edc9495f9acc
SHASHWAT1223 <shashwattewari1223@gmail.com>
2022-07-01 02:27:05 +0530

    first

--- server/index.js
+++ server/index.js
@@ -3,18 +3,21 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const {Server} = require("socket.io");
+const router = require("./router");
+const server = http.createServer(app);
+
 
 app.use(cors());
-
-const server = http.createServer(app);
+app.use(router);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     }
 
 });
+
+
 
 io.on("connection", (socket) => {
 
@@ -37,6 +40,7 @@ io.on("connection", (socket) => {
 });
 
 
-server.listen(3001,() =>{
+
+server.listen(process.env.PORT||3001,() =>{
     console.log("SERVER RUNNING");
 });
